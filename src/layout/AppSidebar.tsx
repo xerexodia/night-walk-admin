e7eb31f95dc35e228c2db1bb027631dd88cc2355
xerexodia <ibrahimfarhat2018@gmail.com>
2025-12-30 21:35:02 +0100

    fix build errors

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -1,11 +1,9 @@
 'use client';
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useSidebar } from '../context/SidebarContext';
 import {
-  BoxCubeIcon,
   ChartUpAndDown,
   ChevronDownIcon,
   GridIcon,
